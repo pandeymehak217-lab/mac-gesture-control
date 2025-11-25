95e2bbedd7a740fc46ecd355bb01ad36daef5fb7
Mehak Pandey <pandeymehak.217@gmail.com>
2025-11-25 23:34:55 +0530

    done
    Removed copyright notice and added module docstring.

--- sounddevice.py
+++ sounddevice.py
@@ -1,22 +1,4 @@
-# Copyright (c) 2015-2023 Matthias Geier
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+
 
 """Play and Record Sound with Python.
 
@@ -2941,11 +2923,10 @@ def _terminate():
 def _exit_handler():
     assert _initialized >= 0
 
-    # We cleanup any open streams here since older versions of portaudio don't
-    # manage this (see github issue #1)
+    
+ 
     if _last_callback:
-        # NB: calling stop() first is required; without it portaudio hangs when
-        # calling close()
+       
         _last_callback.stream.stop()
         _last_callback.stream.close()
 
